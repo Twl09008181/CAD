3f383562133b79227ae54f954825d25bf1b90de8
TimWeiLee <twl0985314726@gmail.com>
2021-03-07 18:01:07 +0800

    整理

--- hw1/TestUnit/main.cpp
+++ hw1/TestUnit/main.cpp
@@ -20,9 +20,11 @@ warning:
 */
 string extract_num(const string&I_O);
 bool binary_search(const vector<int>&V,int target);
-ostringstream& get_N(ostringstream&ostrm,const string&N="",vector<int>*V=nullptr);
+ostringstream& get_N(ostringstream&ostrm,const string&N);
+
 void read(char *argv[],vector<gate>&Gate,vector<int>&Output_V,ostringstream&Input_N,ostringstream&Output_N,ostringstream&Wire_N);
 void print(char *argv[],vector<gate>&Gate,vector<int>&Output_V,ostringstream&Input_N,ostringstream&Output_N,ostringstream&Wire_N);
+
 
 int main(int argc,char *argv[])
 {   
@@ -33,12 +35,13 @@ int main(int argc,char *argv[])
     print(argv,Gate,Output_V,Input_N,Output_N,Wire_N);
     return 0;
 }
+
 void read(char *argv[],vector<gate>&Gate,vector<int>&Output_V,ostringstream&Input_N,ostringstream&Output_N,ostringstream&Wire_N)
 {
     ifstream benchmark{argv[1]};
     if(!benchmark)
     {
-        cerr<<"File cann't' open!"<<endl;
+        cerr<<"Benchmark file cann't' open!"<<endl;
         exit(1);
     }
     string line;
@@ -49,13 +52,17 @@ void read(char *argv[],vector<gate>&Gate,vector<int>&Output_V,ostringstream&Inpu
         count_line++;
         string keyword = get_first_of_line(line);//get the first nonempty words. 
         if(keyword.empty()||keyword.front()=='#')continue;//# or empty line.
+
         if(keyword.find("INPUT")==0)//is INPUT
         {   
             get_N(Input_N,extract_num(line));
         }
         else if (keyword.find("OUTPUT")==0)//is OUTPUT
         {
-            get_N(Output_N,extract_num(line),&Output_V);
+            string n = extract_num(line);
+            get_N(Output_N,n);
+
+            Output_V.push_back(stoi(n));
             un_sort  = true;//假設不一定會照順序排
         }
         else if(isdigit(keyword.at(0)))//is gate
@@ -84,7 +91,7 @@ void print(char *argv[],vector<gate>&Gate,vector<int>&Output_V,ostringstream&Inp
         cerr<<"cann't open "<<argv[2]<<endl;
         exit(1);
     }
-    output_v<<"module "<<split(argv[1],'.').at(0)<<" ("<<Input_N.str()<<Output_N.str()<<");\n"<<endl;
+    output_v<<"module "<<split(argv[1],'.').at(0)<<" ("<<Input_N.str()<<","<<Output_N.str()<<");\n"<<endl;
     output_v<<"input "<<Input_N.str()<<";\n"<<endl<<"output "<<Output_N.str()<<";\n"<<endl<<"wire "<<Wire_N.str()<<";\n"<<endl;
 
     for(int i = 0;i<Gate.size();++i)
@@ -105,13 +112,12 @@ string extract_num(const string&I_O)
     vector<string> tmp = split(I_O,',',i,j,-1,' ');//忽略空格,允許INPUT(   1)
     return tmp.at(0);
 }
-ostringstream& get_N(ostringstream&ostrm,const string&N,vector<int>*V)
+ostringstream& get_N(ostringstream&ostrm,const string&N)
 {
     if(N!=""){
-        if(!ostrm.str().empty())ostrm<<",";
+        if(!ostrm.str().empty())
+            ostrm<<",";
         ostrm<<"N"<<N;
-        if(V)
-            (*V).push_back(stoi(N));
     }
     return ostrm;
 }
