96bf468cd05b0c05c5219602c6e300401f673410
TimWeiLee <twl0985314726@gmail.com>
2021-03-25 16:29:26 +0800

    server 測試ok,main新增print 輸出

--- homework/hw2/main.cpp
+++ homework/hw2/main.cpp
@@ -1,28 +1,76 @@
 #include<iostream>
+#include <sstream>
 #include<fstream>
 #include<string>
 #include<forward_list>
 #include<list>
 using namespace std;
 #include "./lib/DFG.h"
-#include "./lib/UC_schler.h"
+#include "./lib/UC_Schler.h"
 #include "./lib/List_Schler.h"
 
 
 DFG *get_DFG(char *file_name);
 int add_num,mul_num;//resource 數
 
+
+int get_latency(const DFG*dfg ,const vector<int>&Schedule);
+vector<int> get_end_time(const DFG*dfg ,const vector<int>&Schedule);
+
+void print(const DFG*dfg ,const vector<int>&Schedule,string file_name);
+
 int main(int argc,char*argv[])
 {   
     DFG *dfg = get_DFG(argv[1]);
-    
-    
-    vector<int> resource = MR_LCS(dfg,7);
-    for(auto r:resource)
-        cout<<r<<" ";
+    vector<int>R{add_num,mul_num};
+    vector<int>Schedule = ML_RCS(dfg,R);//跑ML_RCS
+    print(dfg,Schedule,string(argv[1])+".out");
     return 0;
 }
+void print(const DFG*dfg ,const vector<int>&Schedule,string file_name)
+{
+    ofstream output_file{file_name};
+    if(!output_file){cerr<<"cann't open"<<file_name<<endl;exit(1);}
+    
+    
+    int latency = get_latency(dfg,Schedule);
+    output_file<<latency<<"\n";
 
+
+    vector<int>end_time = get_end_time(dfg,Schedule);
+    vector<vector<int>>each_time_sch(latency,vector<int>{});
+    for(int i = 0;i<Schedule.size();i++)
+        for(int t = Schedule[i];t<end_time[i];t++)
+            each_time_sch.at(t).push_back(i);
+    
+    const auto&V = dfg->get_node_vector();
+    for(const auto&each_time:each_time_sch)
+    {
+        ostringstream each_line;
+        for(auto n:each_time)
+        {
+            if(!each_line.str().empty())each_line<<" ";
+            each_line<<V.at(n).get_val();
+        }
+        each_line<<"\n";
+        output_file<<each_line.str();
+    }
+    output_file.close();
+}
+int get_latency(const DFG*dfg ,const vector<int>&Schedule)
+{
+    int latency = 0;
+    for(auto o:dfg->get_output_index())
+        latency = max(Schedule.at(o),latency);
+    return latency;
+}
+vector<int> get_end_time(const DFG*dfg ,const vector<int>&Schedule)
+{
+    vector<int>end_time(Schedule.size(),0);
+    for(int i = 0;i<end_time.size();i++)
+        end_time.at(i) = Schedule.at(i) + dfg->get_node_vector().at(i).get_delay();
+    return end_time;
+}
 
 DFG *get_DFG(char *file_name)
 {
