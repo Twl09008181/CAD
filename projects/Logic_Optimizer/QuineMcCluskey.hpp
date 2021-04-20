19b14c8f18b4cdb9dbcb9488c46da6cf554da9a7
Twl09008181 <Twl09008181@gmail.com>
2021-04-20 21:17:33 +0800

    將回傳Prime用std::vector回傳

--- projects/Logic_Optimizer/QuineMcCluskey.hpp
+++ projects/Logic_Optimizer/QuineMcCluskey.hpp
@@ -17,11 +17,11 @@ using Function = std::vector<std::vector<int>>;
 // {15} };  //has four 1
 
 
-std::set<Implicant> Prime_Generate(Function &f);//Input function , return Prime_implicants.
-std::set<Implicant> Min_Cover(Function &f,std::set<Implicant>&prime);//Use smallest prime_implicants to cover function f,and return those implicants. 
-inline std::set<Implicant> QuineMcCluskey(Function&f)
+std::vector<Implicant> Prime_Generate(Function &f);//Input function , return Prime_implicants.
+std::vector<Implicant> Min_Cover(Function &f,std::vector<Implicant>&prime);//Use smallest prime_implicants to cover function f,and return those implicants. 
+inline std::vector<Implicant>  QuineMcCluskey(Function&f)
 {
-    std::set<Implicant> prime = Prime_Generate(f);//generate prime implicants
+    std::vector<Implicant> prime = Prime_Generate(f);//generate prime implicants
     return Min_Cover(f,prime);//cover
 }
 
