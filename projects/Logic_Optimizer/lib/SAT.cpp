2fc92832e9e09f66f0d0b52407f8f02d801d2da7
Twl09008181 <Twl09008181@gmail.com>
2021-04-21 23:52:35 +0800

    更動一些inline

--- projects/Logic_Optimizer/lib/SAT.cpp
+++ projects/Logic_Optimizer/lib/SAT.cpp
@@ -1,4 +1,33 @@
 #include "SAT.hpp"
+
+
+void SAT::add_bracket(const bracket &br)
+{
+    bracket new_br;
+    new_br.reserve(br.size());
+
+    for(auto l : br)
+    {
+        if(lit_id.find(l)==lit_id.end())//l is a new literal
+        {
+            lit_id.insert({l,literals.size()});
+            literals.push_back({l});
+        }
+        new_br.push_back(lit_id[l]);//put index of l in std::vector<literal>literals into bracket. used in bool SAT::evaulate_one_bracket(const bracket& br).
+    }
+    brackets.push_back(new_br);
+}
+
+std::vector<int> SAT::min_cover_SAT()//return the minimum solution that can SAT.
+{
+    int min_one = literals.size();
+    std::vector<int>ans(literals.size());
+    for(int i = 0;i < ans.size(); i++)//set the worst solution : all literals = 1
+        ans[i] = literals[i].id;
+    
+    SAT_solver(0,ans);
+    return ans;
+}
 void SAT::SAT_solver(int i,std::vector<int>&ans,std::vector<int>current)
 {
 
@@ -19,21 +48,4 @@ void SAT::SAT_solver(int i,std::vector<int>&ans,std::vector<int>current)
         current.push_back(literals[i].id);
         SAT_solver(i+1,ans,current);//set li = true
     }
-}
-
-void SAT::add_bracket(const bracket &br)
-{
-    bracket new_br;
-    new_br.reserve(br.size());
-
-    for(auto l : br)
-    {
-        if(lit_id.find(l)==lit_id.end())//l is a new literal
-        {
-            lit_id.insert({l,literals.size()});
-            literals.push_back({l});
-        }
-        new_br.push_back(lit_id[l]);//put index of l in std::vector<literal>literals into bracket. used in bool SAT::evaulate_one_bracket(const bracket& br).
-    }
-    brackets.push_back(new_br);
 }
--- projects/Logic_Optimizer/lib/SAT.hpp
+++ projects/Logic_Optimizer/lib/SAT.hpp
@@ -22,13 +22,13 @@ public:
         literals.reserve(literal_len);
     }
 
-    void add_bracket(const bracket &br);//interface,but not efficent.
-    inline std::vector<int> min_cover_SAT();
+    void add_bracket(const bracket &br);//an interface for adding bracket like (p1+p2+p7), need input {1,2,7}
+    std::vector<int> min_cover_SAT();
     
 private:
     inline bool evaulate_one_bracket(const bracket& br);
     inline bool evaulate_SAT();
-    void SAT_solver(int i,std::vector<int>&ans,std::vector<int>current={});
+    void SAT_solver(int i,std::vector<int>&ans,std::vector<int>current={});//recursive solve.
 
 
     std::map<int,int>lit_id;//every literal in SAT is unique, use id mapping into the index in std::vector<literal>literals.
@@ -36,16 +36,7 @@ private:
     std::vector<bracket> brackets;//overall Product of sum of literals.
 };
 
-inline std::vector<int> SAT::min_cover_SAT()//return the minimum solution that can SAT.
-{
-    int min_one = literals.size();
-    std::vector<int>ans(literals.size());
-    for(int i = 0;i < ans.size(); i++)//set the worst solution : all literals = 1
-        ans[i] = literals[i].id;
-    
-    SAT_solver(0,ans);
-    return ans;
-}
+
 
 inline bool SAT::evaulate_one_bracket(const bracket& br)//OR
 {
