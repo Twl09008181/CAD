366f8429f76319622d7f535d7b0a3c6838f54ae4
Twl09008181 <Twl09008181@gmail.com>
2021-04-22 23:59:55 +0800

    新增註解

--- projects/Logic_Optimizer/lib/QuineMcCluskey.hpp
+++ projects/Logic_Optimizer/lib/QuineMcCluskey.hpp
@@ -8,8 +8,9 @@
 //--------------------------Data structure this lib use--------------------------
 #include "Function.hpp"
 #include "Implicant.hpp"
-#include "Prime_Implicant_Chart.hpp"
-using Implicant_Combine_table = std::vector<std::map <Implicant,bool>>;
+using Implicant_Combine_table = std::vector<std::map <Implicant,bool>>;//use in phase 1 : find prime_implicants
+#include "Prime_Implicant_Chart.hpp"//use in phase2: find essential prime implcaints and cover terms by ESPI.
+
 
 //--------------------------Petrick’s Method-------------------------------------
 #include "SAT.hpp"
@@ -19,8 +20,8 @@ using Implicant_Combine_table = std::vector<std::map <Implicant,bool>>;
 #include <iomanip>
 
 //-----------------------------Interface-------------------------------------------
-std::vector<Implicant> Prime_Generate(Function &f,bool show_procedure = false);//Input function , return Prime_implicants.
-std::vector<Implicant> Min_Cover(Function &f,std::vector<Implicant>&prime);//Use smallest prime_implicants to cover function f,and return those implicants. 
+std::vector<Implicant> Prime_Generate(Function &f,bool show_procedure = false);//phase 1 : Input function , return Prime_implicants.
+std::vector<Implicant> Min_Cover(Function &f,std::vector<Implicant>&prime);//phase 2 : Use smallest prime_implicants to cover function f,and return those implicants. 
 inline std::vector<Implicant>  QuineMcCluskey(Function&f)
 {
     std::vector<Implicant> prime = Prime_Generate(f);//generate prime implicants
