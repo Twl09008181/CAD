bb739251587ebd7379e00aa4e18d8a186b6e87be
Twl09008181 <Twl09008181@gmail.com>
2021-04-24 16:13:21 +0800

    Input_Interface開發完成

--- projects/Logic_Optimizer/Input_Interface/Input.h
+++ projects/Logic_Optimizer/Input_Interface/Input.h
@@ -4,40 +4,53 @@
 
 #include "../lib/Function.hpp"
 
-#include <vector>
+
 #include <string>
 #include <algorithm>
 #include <utility>
 
-// inline std::pair<unsigned int,unsigned int> str_to_implicant_val(std::string::const_iterator &s,std::string::const_iterator &e)
-// {
-//     unsigned int val = 0;
-//     while(s != e)
-//     {
-//         auto literal_cut = find(s,e,'*');
-//         auto literal = find(s,literal_cut,'!');
-        
-//         if(literal!=literal_cut)//need add
-//             ;
-        
-//         s = literal_cut + 1;
 
-//     }
-// }
+
+inline std::pair<unsigned int,unsigned int> str_to_implicant_val(std::string::const_iterator s,std::string::const_iterator e,unsigned int exp)
+{
+    unsigned int val = 0;
+    unsigned int num_of_one = 0;
+    while(s != e)
+    {
+        s++;//we need ++ in case of infinite loop.
+
+        auto literal_cut = find(s,e,'*');//each literal splited by '*'
+
+        auto literal = find(s, literal_cut,'!');//if '!' is exist , it is a negative literal.
+        
+        if(literal == literal_cut)//it is a positive literal
+        {
+            num_of_one ++;
+            val += exp;
+        }
+        exp/=2;
+        s = literal_cut;// s is the position of '*'
+    }
+    return std::make_pair(val,num_of_one);
+}
 
 inline Function get_function(const std::string&str,int Fan_in)
 {
     Function F;F.resize(Fan_in + 1);//also need 0 one's field.
-    unsigned int max_num_of_one = 0;
 
+    unsigned int exp = 1;
+    for(int i = 0;i<Fan_in - 1;i++)
+        exp*=2;
+ 
+    unsigned int max_num_of_one = 0;
 
     auto open = find(str.begin(),str.end(),'(');
 
     while(open != str.end())
     {
         auto close = find(open,str.end(),')');
-        
-        auto result = str_to_implicant_val(open, close);//return val and the number of bits which are 1 in val.
+      
+        auto result = str_to_implicant_val(open, close,exp);//return val and the number of bits which are 1 in val.
         
         F.at(result.second).push_back(result.first);
 
@@ -54,10 +67,5 @@ inline Function get_function(const std::string&str,int Fan_in)
     return F;
 }
 
-// int main()
-// {
 
-
-
-// }
 #endif
